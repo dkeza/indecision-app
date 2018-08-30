5875fad59daca783a66330193a03a352f7ad0178
keza <dk@citadel.rs>
2018-08-30 23:15:41 +0200

    Add components methods

--- src/app.js
+++ src/app.js
@@ -26,19 +26,26 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+    handlePick() {
+        alert('handlePick');
+    }
     render() {
         return (
             <div>
-                <button>What should I do?</button>
+                <button onClick={this.handlePick}>What should I do?</button>
             </div>
         );
     }
 }
 
 class Options extends React.Component {
+    handlRemoveAll() {
+        alert('handlRemoveAll');
+    }
     render() {
         return (
             <div>
+                <button onClick={this.handlRemoveAll}>Remove all</button>
                 {
                 this.props.options.map( (option) => <Option key={option} optionText={option}/>)
                 }
@@ -58,10 +65,22 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+    handleAddOption(e) {
+        e.preventDefault();
+
+        const option = e.target.elements.option.value;
+    
+        if (option) {
+            alert('something entered');
+        }
+    }
     render() {
         return (
             <div>
-            AddOption component here
+                <form onSubmit={this.handleAddOption}>
+                    <input type="text" name="option"/>
+                    <button>Add Option</button>
+                </form>
             </div>
         );
     }
